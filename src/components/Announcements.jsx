6099605820944364dcd1f46f383bae9b783f8b46
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-22 04:24:05 +0530

    Create Announcements.jsx

---
+++ src/components/Announcements.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+const Announcements = () => {
+  return (
+    <Container>BLACK FRIDAY SALE! $50 off on all leather shoes!</Container>
+  );
+};
+
+export default Announcements;
