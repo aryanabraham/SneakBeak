e0d958e0c9df4a9c213c2d260fe70395e7877e40
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-23 03:51:37 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/1032110/pexels-photo-1032110.jpeg",
+    title: "BLACK FRIDAY SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fff700",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/3146180/pexels-photo-3146180.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "CONVERSE CRAZE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "209b39",
+  },
+  {
+    id: 3,
+    img: "https://cdn-us.christianlouboutin.com/media/wysiwyg/TOP_BOX_COVER_US_CA_ASIA_JP.jpg",
+    title: "LOUBOUITON LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "9d0c0c",
+  },
+];
