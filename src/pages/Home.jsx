092a428cfd3be77a90a9d1dc036d1693d1bec240
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-24 03:48:23 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Announcements from "../components/Announcements";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <div>
       <Announcements />
       <Navbar />
+      <Slider />
+      <Categories />
     </div>
   );
 };
