25bd0e136cb3a28df3e9d7185325a8f30529c68d
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-27 04:43:17 +0530

    Create Newsletter.jsx

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,62 @@
+import { SendOutlined } from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+
+const Desc = styled.div`
+  font-size: 24px;
+  font-weight: 300px;
+  margin-bottom: 20px;
+`;
+
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 3px solid white;
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
+
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+`;
+
+const Newsletter = () => {
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Desc>Get updated whenever the latest stock drops.</Desc>
+      <InputContainer>
+        <Input placeholder="email@domain.com" />
+        <Button>
+          <SendOutlined />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Newsletter;
