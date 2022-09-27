e5dacf82df434d0c46fdad1dc7eccdcfa92a4224
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-28 03:53:10 +0530

    Create ProductList.jsx

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Title>Sneakers</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>UK 5 </Option>
+            <Option>UK 6</Option>
+            <Option>UK 7</Option>
+            <Option>UK 8</Option>
+            <Option>UK 9</Option>
+            <Option>UK 10</Option>
+            <Option>UK 11</Option>
+            <Option>UK 12</Option>
+            <Option>UK 13</Option>
+            <Option>UK 14</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (High - Low)</Option>
+            <Option>Price (Low - High)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
