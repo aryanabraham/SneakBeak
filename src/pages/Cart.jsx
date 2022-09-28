9349e3267f98b9daf23893e02613f0ac843742b6
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-29 03:24:48 +0530

    Create Cart.jsx

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,259 @@
+import {
+  AddCircleOutlineRounded,
+  RemoveCircleOutlineRounded,
+} from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  font-weight: 400;
+  text-align: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TopTexts = styled.div``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid black;
+  border-radius: 10px;
+  padding: 20px;
+  margin-top: 15px;
+  margin-left: 15px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1``;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "600"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductID = styled.span``;
+
+const ProductColorContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin: 0px 5px;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const hR = styled.hr`
+  background-color: #eee;
+  border: none;
+  margin: 5px 0px;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist(0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/950adf08-e1fa-43e6-9203-b225b74655f5/air-force-1-high-07-lv8-shoe-tCJTql.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Nike Air Force 1 High '07 LV8
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>9558523035
+                  </ProductID>
+                  <ProductColorContainer>
+                    <ProductColor color="black" />
+                    <ProductColor color="#5f408c" />
+                  </ProductColorContainer>
+                  <ProductSize>
+                    <b>Size: </b>UK 10
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <RemoveCircleOutlineRounded />
+                  <ProductAmount>2</ProductAmount>
+                  <AddCircleOutlineRounded />
+                </ProductAmountContainer>
+                <ProductPrice>$139 </ProductPrice>
+              </PriceDetail>
+            </Product>
+            <hR></hR>
+            <Product>
+              <ProductDetail>
+                <Image src="https://images.hugoboss.com/is/image/boss/hbeu50370447_001_250?$large$=&fit=crop,1&align=1,1&wid=768&qlt=80&fmt=webp" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>BOSS Oxford Shoes
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>8092081075
+                  </ProductID>
+                  <ProductColorContainer>
+                    <ProductColor color="black" />
+                  </ProductColorContainer>
+                  <ProductSize>
+                    <b>Size: </b>UK 10
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <RemoveCircleOutlineRounded />
+                  <ProductAmount>1</ProductAmount>
+                  <AddCircleOutlineRounded />
+                </ProductAmountContainer>
+                <ProductPrice>$299 </ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal:</SummaryItemText>
+              <SummaryItemPrice>$89</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping:</SummaryItemText>
+              <SummaryItemPrice>$4.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount:</SummaryItemText>
+              <SummaryItemPrice>-$4.99</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total:</SummaryItemText>
+              <SummaryItemPrice>$399</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
