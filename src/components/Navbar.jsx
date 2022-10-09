680d22863f9a46df2a4d7bb24f8e9ba3104c7205
Aryan Abraham <abraham.aryan01@gmail.com>
2022-10-10 04:40:05 +0530

    Made Navbar Mobile-friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@mui/icons-material";
 import { Badge } from "@mui/material";
+import { mobile } from "../responsive";
+
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px", width: "50vh" })}
 `;
 
 const Wrapper = styled.div`
@@ -13,6 +16,7 @@ const Wrapper = styled.div`
   justify-content: space-between;
   background-color: black;
   color: white;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
@@ -24,6 +28,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
 const SearchContainer = styled.div`
@@ -32,19 +37,24 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
+  cursor: pointer;
+  ${mobile({ marginLeft: "5px", paddingRight: "0px" })}
 `;
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
   flex: 1;
   text-align: center;
+  cursor: pointer;
 `;
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
@@ -52,12 +62,18 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({
+    flex: 2,
+    justifyContent: "flex-start",
+    marginLeft: "0px",
+  })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "7px" })}
 `;
 
 const Navbar = () => {
@@ -67,7 +83,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
