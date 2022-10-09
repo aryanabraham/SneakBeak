5de4ff9fb82907f34c0a939e9062161939e35e68
Aryan Abraham <abraham.aryan01@gmail.com>
2022-10-10 04:48:33 +0530

    Made ProductList Mobile-friendly

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Footer from "../components/Footer";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
@@ -18,18 +19,21 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: "0px 20px", display: "flex", flexDirection: "column" })}
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
   cursor: pointer;
+  ${mobile({ margin: "10px 0px" })}
 `;
 
 const Option = styled.option``;
@@ -38,7 +42,7 @@ const ProductList = () => {
   return (
     <Container>
       <Navbar />
-      <Title>Shoes</Title>
+      <Title>Products</Title>
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products</FilterText>
