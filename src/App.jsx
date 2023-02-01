5e8b1e7b60432db8abd33a8c55798d07b611ffc9
Aryan Abraham <abraham.aryan01@gmail.com>
2023-02-02 00:40:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
-
+import Home from './pages/home';
+import React from 'react'
+import Register from './pages/Register'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import ProductList from './pages/ProductList';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 const App = () => {
-  return <Cart />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/Register" element={<Register/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/Cart" element={<Cart/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/Login" element={<Login/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/Product" element={<Product/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/ProductList" element={<ProductList/>}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
