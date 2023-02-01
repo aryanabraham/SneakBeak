13b3807f10d1fd03327f6580e97a1219b3a0e35a
Aryan Abraham <abraham.aryan01@gmail.com>
2023-02-02 02:17:19 +0530

    Added React Scroll Progress Bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,18 @@ import Categories from "../components/Categories";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import Bot from "../components/Chatbot.jsx";
+import ProgressBar from "react-scroll-progress-bar";
+
 const Home = () => {
   return (
     <div>
+      <ProgressBar height="7px" duration="0.2"/>
       <Navbar />
       <Slider />
       <Categories />
       <Products />
+      <Bot />
       <Newsletter />
       <Footer />
     </div>
